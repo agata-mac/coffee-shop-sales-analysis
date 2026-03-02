0e34ee8b2c55dd433ccf57f90e4d44986a6a8548
agata-mac <agatamaczynska7@gmail.com>
2026-03-02 12:56:12 +0100

    Created coffe_shop_sales table schema and imported data

---
+++ 01_schema.sql
@@ -0,0 +1,16 @@
+-- Coffee Shop Sales Analysis
+-- Table schema
+
+CREATE TABLE coffee_shop_sales (
+    transaction_id     NUMBER PRIMARY KEY,
+    transaction_date   DATE,
+    transaction_time   VARCHAR2(8),
+    transaction_qty    NUMBER,
+    store_id           NUMBER,
+    store_location     VARCHAR2(50),
+    product_id         NUMBER,
+    unit_price         NUMBER(5, 2),
+    product_category   VARCHAR2(50),
+    product_type       VARCHAR2(50),
+    product_detail     VARCHAR2(100)
+);
