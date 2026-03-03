7db3ce20e58994a4f55618281b1918fe29af897f
agata-mac <agatamaczynska7@gmail.com>
2026-03-03 21:24:37 +0100

    Inserted data into stores table.

--- 02_data.sql
+++ 02_data.sql
@@ -14,4 +14,15 @@ INSERT INTO products (
     FROM
         coffee_shop_sales;
         
+-- Insert data into stores table.
+
+INSERT INTO stores (
+    store_id,
+    store_location
+)
+    SELECT DISTINCT
+        store_id,
+        store_location
+    FROM
+        coffee_shop_sales;
   
