ef6993930c8884306167a4f06b7b15c98d9beb1d
agata-mac <agatamaczynska7@gmail.com>
2026-03-06 11:43:22 +0100

    Add monthly revenue analysis.

--- 03_analysis.sql
+++ 03_analysis.sql
@@ -8,3 +8,12 @@ FROM
     sales
 GROUP BY
     transaction_date;
+    
+-- Calculate monthly revenue.
+SELECT
+    TO_CHAR(transaction_date, 'YYYY-MM') AS month,
+    SUM(transaction_qty * unit_price) AS revenue
+FROM
+    sales
+GROUP BY
+    TO_CHAR(transaction_date, 'YYYY-MM');
